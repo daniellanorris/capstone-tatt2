bd9516be3b54ba428b9e4c2c5da9d1a59be066a3
daniellanorris <daniellanorris@ufl.edu>
2023-11-14 08:03:16 -0700

    added home page styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,10 @@ import { useUserData } from '../context/userContext';
 import GeoLocationData from '../components/geolocationData';
 import placeholder from 'public/placeholder.jpeg'
 
+
+//need to implement scrolling functionality with this react pkg https://www.npmjs.com/package/react-scroll
+
+
 export default function Home() {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
@@ -15,6 +19,9 @@ export default function Home() {
     const { savedArtists, setSavedArtists } = useUserData();
     const [tattooStyle, setIsTattooStyle] = useState(false);
     const [selectedButtons, setSelectedButtons] = useState(false);
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth > 750)
+    const [selectedArtist, setSelectedArtist] = useState(null);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -60,10 +67,13 @@ export default function Home() {
                 const artistData = await artistResponse.json();
 
 
+
+
                 setData(userData)
 
                 if (Array.isArray(userData.data) && userData.data.length > 0) {
                     console.log('artistData:', userData.data);
+
 
                     // Access the first item's _id
                     console.log('First artist _id:', userData.data[0]._id);
@@ -73,6 +83,7 @@ export default function Home() {
 
 
                 setData(artistData);
+                setSelectedArtist(artistData.data[0])
 
                 if (Array.isArray(artistData.data) && artistData.data.length > 0) {
                     console.log('artistData:', artistData.data);
@@ -98,12 +109,14 @@ export default function Home() {
         fetchData();
     }, [setUserId]);
 
+
     const changeButtonColor = (buttonName) => {
         setSelectedButtons((prevState) => ({
             ...prevState,
             [buttonName]: !prevState[buttonName],
         }));
     };
+
     async function saveArtist(artistId, userId) {
         console.log('saveArtist function called');
         console.log('artistId:', artistId);
@@ -150,8 +163,24 @@ export default function Home() {
         setIsTattooStyle((prevState) => !prevState);
     }
 
+    async function setSize() {
+        setIsDesktop(window.innerWidth > 750)
+
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", setSize)
+        return () => window.removeEventListener("resize", setSize)
+    });
+
+    const setArtist = (index) => {
+        setSelectedArtist(data.data[index]);
+        console.log(selectedArtist)
+
+    };
 
     return (
+
         <div style={{ marginLeft: "10px" }}>
             {isLoggedIn ? (
                 <div>
@@ -229,52 +258,153 @@ export default function Home() {
 
 
                     {data && data.data && (
-                        <div>
-                            <h3>Click on each card to access artist profile pages!</h3>
-                            {error ? (
-                                <p>Error fetching artists: {error}</p>
-                            ) : (
-                                <ul style={{ padding: "0px" }}>
-                                    {data.data.map((item, index) => (
-                                        <div key={index} className="card m-4 d-flex justify-content-end">
-
-                                            <Link href="/artist/[artistId]" as={`/artist/${item._id}`}>
-
-                                                <h3 className="custom-card-header">
-                                                    {item.firstname} {item.lastname}
-                                                </h3>
-
-                                                <div style={{ borderRadius: "50%", border: "8px solid orange", overflow: "hidden", width: 100, height: 100 }} class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                                                    <img src={item.profilePicture ? item.profilePicture : placeholder} width={100} height={100} alt={`${item.firstname} ${item.lastname}`} />
-                                                </div>
-                                                <p>@{item.username}</p>
-                                                <p>Location: {item.location} </p>
-                                                {item.tattooStyle && item.tattooStyle.length > 0 && (
-                                                    <div>
-                                                        <p>Tattoo Styles:</p>
-                                                        <ul>
-                                                            {item.tattooStyle.map((style, styleIndex) => (
-                                                                <li key={styleIndex}>{style}</li>
-                                                            ))}
-                                                        </ul>
+                        isDesktop ? (
+                            <div className="container">
+                                <div className="row">
+                                    <h3>Click on each card to access artist profile pages!</h3>
+                                    <div className="col-5">
+                                        {error ? (
+                                            <p>Error fetching artists: {error}</p>
+                                        ) : (
+                                            <ul style={{ padding: "0px" }}>
+                                                {data.data.map((item, index) => (
+                                                    <div key={index} onClick={() => setArtist(index)} className="card mt-4 d-flex justify-content-end">
+                                                        <h3 className="custom-card-header">
+                                                            {item.firstname} {item.lastname}
+                                                        </h3>
+                                                        <div style={{ borderRadius: "50%", border: "8px solid orange", overflow: "hidden", width: 100, height: 100 }} className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                                                            <img src={item.profilePicture ? item.profilePicture : placeholder} width={100} height={100} alt={`${item.firstname} ${item.lastname}`} />
+                                                        </div>
+                                                        <p>@{item.username}</p>
+                                                        <p>Location: {item.location} </p>
+                                                        {item.tattooStyle && item.tattooStyle.length > 0 && (
+                                                            <div>
+                                                                <p>Tattoo Styles:</p>
+                                                                <ul>
+                                                                    {item.tattooStyle.map((style, styleIndex) => (
+                                                                        <li key={styleIndex}>{style}</li>
+                                                                    ))}
+                                                                </ul>
+                                                            </div>
+                                                        )}
+                                                        {isUser ? (
+                                                            <button
+                                                                style={{ width: "50%" }}
+                                                                className={`button ${savedArtists && savedArtists[item._id] ? 'selected' : ''}`}
+                                                                onClick={() => saveArtist(item._id, userId)}
+                                                            >
+                                                                {savedArtists && savedArtists[item._id] ? 'Saved Artist' : 'Save Artist'}
+                                                            </button>
+                                                        ) : null}
                                                     </div>
-                                                )}
+                                                ))}
+                                            </ul>
+                                        )}
+                                    </div>
+                                    <div className="col-7 mt-4 dark-card">
+                                        <div className="card mt-4 d-flex justify-content-end">
+                                            <div className="col-9">
+                                                <ul style={{ padding: "0px" }}>
+                                                    <div className="card m-4 d-flex justify-content-end">
+                                                        <Link href="/artist/[artistId]" as={`/artist/${selectedArtist._id}`}>
+                                                            <h3 className="custom-card-header">
+                                                                {selectedArtist.firstname} {selectedArtist.lastname}
+                                                            </h3>
+                                                            <div style={{ borderRadius: "50%", border: "8px solid orange", overflow: "hidden", width: 100, height: 100 }} className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                                                                <img src={selectedArtist.profilePicture ? selectedArtist.profilePicture : placeholder} width={100} height={100} alt={`${selectedArtist.firstname} ${selectedArtist.lastname}`} />
+                                                            </div>
+                                                            <p>@{selectedArtist.username}</p>
+                                                            <p>Location: {selectedArtist.location} </p>
+                                                            {selectedArtist.tattooStyle && selectedArtist.tattooStyle.length > 0 && (
+                                                                <div>
+                                                                    <p>Tattoo Styles:</p>
+                                                                    <ul>
+                                                                        {selectedArtist.tattooStyle.map((style, styleIndex) => (
+                                                                            <li key={styleIndex}>{style}</li>
+                                                                        ))}
+                                                                    </ul>
+                                                                </div>
+                                                            )}
+                                                        </Link>
+                                                        {isUser ? (
+                                                            <button
+                                                                style={{ width: "50%" }}
+                                                                className={`button ${savedArtists && savedArtists[selectedArtist._id] ? 'selected' : ''}`}
+                                                                onClick={() => saveArtist(selectedArtist._id, userId)}
+                                                            >
+                                                                {savedArtists && savedArtists[selectedArtist._id] ? 'Saved Artist' : 'Save Artist'}
+                                                            </button>
+                                                        ) : null}
+                                                    </div>
 
-                                            </Link>
-                                            {isUser ? (
-                                                <button
-                                                    style={{ width: "50%" }}
-                                                    className={`button ${savedArtists && savedArtists[item._id] ? 'selected' : ''}`}
-                                                    onClick={() => saveArtist(item._id, userId)}
-                                                >
-                                                    {savedArtists && savedArtists[item._id] ? 'Saved Artist' : 'Save Artist'}
-                                                </button>
-                                            ) : null}
+                                                </ul>
+                                            </div>
                                         </div>
-                                    ))}
-                                </ul>
-                            )}
-                        </div>
+                                        <div className="card m-4 d-flex justify-content-end">
+                                            <div className="row">
+                                                {Object.values(selectedArtist.image).map((imageUrl, index) => (
+                                                    <div key={index} className="col-lg m-3">
+                                                        <div className="card image-container">
+                                                            <img src={imageUrl} alt={`Image ${index}`} className="card-img-top img-fluid" />
+                                                        </div>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
+                                       
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="container">
+                                <div className="row">
+                                    <h3>Click on each card to access artist profile pages!</h3>
+                                    <div className="col-10">
+                                        {error ? (
+                                            <p>Error fetching artists: {error}</p>
+                                        ) : (
+                                            <ul style={{ padding: "0px" }}>
+                                                {data.data.map((item, index) => (
+                                                    <div key={index} className="card m-4 d-flex justify-content-end">
+                                                        <Link href="/artist/[artistId]" as={`/artist/${item._id}`}>
+                                                            <h3 className="custom-card-header">
+                                                                {item.firstname} {item.lastname}
+                                                            </h3>
+                                                            <div style={{ borderRadius: "50%", border: "8px solid orange", overflow: "hidden", width: 100, height: 100 }} className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                                                                <img src={item.profilePicture ? item.profilePicture : placeholder} width={100} height={100} alt={`${item.firstname} ${item.lastname}`} />
+                                                            </div>
+                                                            <p>@{item.username}</p>
+                                                            <p>Location: {item.location} </p>
+                                                            {item.tattooStyle && item.tattooStyle.length > 0 && (
+                                                                <div>
+                                                                    <p>Tattoo Styles:</p>
+                                                                    <ul>
+                                                                        {item.tattooStyle.map((style, styleIndex) => (
+                                                                            <li key={styleIndex}>{style}</li>
+                                                                        ))}
+                                                                    </ul>
+                                                                </div>
+                                                            )}
+                                                        </Link>
+                                                        {isUser ? (
+                                                            <button
+                                                                style={{ width: "50%" }}
+                                                                className={`button ${savedArtists && savedArtists[item._id] ? 'selected' : ''}`}
+                                                                onClick={() => saveArtist(item._id, userId)}
+                                                            >
+                                                                {savedArtists && savedArtists[item._id] ? 'Saved Artist' : 'Save Artist'}
+                                                            </button>
+                                                        ) : null}
+                                                    </div>
+                                                ))}
+                                            </ul>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        )
                     )}
                     {message && <p>{message}</p>}
                 </div>
@@ -283,9 +413,11 @@ export default function Home() {
                     <h3>Whoa, there! </h3>
                     <p>You are not logged in. Please log in.</p>
                     <Link href="/login">
-                        <button>Login</button></Link>
+                        <button>Login</button>
+                    </Link>
                 </div>
             )}
         </div>
     );
+
 }
