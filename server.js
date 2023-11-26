2e0765bfec45b4a0a02170dda0aa51ac26ee3984
daniellanorris <daniellanorris@ufl.edu>
2023-11-26 10:29:13 -0700

    debug

---
+++ server.js
@@ -0,0 +1,22 @@
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  // Custom server-side route for redirection
+  server.get('/old-route', (req, res) => {
+    res.redirect('/new-route');
+  });
+
+  // Default Next.js handler
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(3000, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
