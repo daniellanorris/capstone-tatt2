f89cc0f12cafcc5f81a9e462828139cea507f0f9
daniellanorris <daniellanorris@ufl.edu>
2023-12-03 17:14:11 -0700

    s3 updates

--- components/imageForm.jsx
+++ components/imageForm.jsx
@@ -16,8 +16,8 @@ const ImageUploadForm = () => {
     }
   
     AWS.config.update({
-      accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS,
-      secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       region: 'us-east-1',
     });
   
--- components/profileForms.jsx
+++ components/profileForms.jsx
@@ -16,8 +16,8 @@ const ProfileUploadForm = () => {
 
   const handleUpload = () => {
     AWS.config.update({
-      accessKeyId: process.env.AWS_ACCESS,
-      secretAccessKey: process.env.AWS_SECRET,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       region: 'us-east-1',
     });
 
